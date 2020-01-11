57e4da71754975a93542eb865f704761c410e5c0
Samuel Louis <sampeur4life1@gmail.com>
2020-01-10 22:59:20 -0300

    a program that reads integer numbers and stores them in a vector then shows on screen all negative numbers read

---
+++ Vector2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+/// <summary>
+/// a program that reads integer numbers and stores them in a vector then shows on screen all negative numbers read
+/// using Split
+/// </summary>
+
+namespace Vector2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new int[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(s[i]);
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i]);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
