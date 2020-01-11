d8d94b3cc75009c5a8bfcfa77913cc38d3b7903f
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 00:15:01 -0300

    its a program that shows students' details, then occupied rooms in a camp

---
+++ RentStudentCamp/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace RentHouse
+{
+    class Student
+    {
+        public string _name { get; set; }
+        public string Email { get; set; }
+
+        public Student(string name, string email)
+        {
+            _name = name;
+            Email = email;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value != null && value.Length > 2)
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return _name + ", " + Email;
+        }
+    }
+}
