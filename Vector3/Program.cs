177fb2ce0b8a44b129f4cdc7fe29161e0c9a89df
Samuel Louis <sampeur4life1@gmail.com>
2020-01-10 23:09:27 -0300

    a program that show on screen the sum and average of the vector elements

---
+++ Vector3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+/// <summary>
+/// a program that reads N real numbers and stores them in a vector next: 
+/// Print all vector elements show on screen the sum and average of the vector elements
+/// </summary>
+
+namespace Vector3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N;
+            double[] A;
+
+            N = int.Parse(Console.ReadLine());
+            A = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = double.Parse(s[i], CultureInfo.InvariantCulture);
+            }
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write(A[i] + " ");
+            }
+            Console.WriteLine("");
+
+            double sum = 0.0;
+            for (int i = 0; i < N; i++)
+            {
+                sum = sum + A[i];
+            }
+            double average = sum / N;
+
+            Console.WriteLine(sum.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(average.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
