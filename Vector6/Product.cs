9b255d6776a648068b160618c84c9ee44fb4246d
Samuel Louis <sampeur4life1@gmail.com>
2020-01-10 23:59:25 -0300

    its a program that shows show the average price of the products

--- Vector6/Product.cs
+++ Vector6/Product.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Vector6
+﻿namespace Vector6
 {
     class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
     }
 }
