104b018144916723c1e881c37ae0c0cca0ccc70d
Samuel Louis <sampeur4life1@gmail.com>
2020-01-11 00:08:28 -0300

    its a program that reads an N value and then the name of N people.Then show the names read

--- Vector7/Program.cs
+++ Vector7/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace Vector7
+
+/// a program to read an N value and then the name of N people.Then show the names read.
+
+
+/// <summary> o mesmo exemplo usando foreach
+/// foreach (string elemento in vet)
+/// {
+///     Console.WriteLine(elemento);
+/// }
+/// </summary>
+
+namespace ForEach
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int N;
+            N = int.Parse(Console.ReadLine());
+            string[] vect = new string[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                vect[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Names read: ");
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
