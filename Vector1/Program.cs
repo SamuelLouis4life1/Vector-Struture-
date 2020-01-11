3bf4059a70e4671ae24b6aa2996ab21c7fb83c04
Samuel Louis <sampeur4life1@gmail.com>
2020-01-10 22:34:59 -0300

    inicial commit on my first projects about vertor in programming logic

---
+++ Vector1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+/// <summary>
+/// a program to read a positive integer N, then any N numbers and store them in a vector. 
+/// Then show all vector elements on screen
+/// </summary>
+
+namespace VectorsStruture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, i;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine());
+            vet = new double[N];
+
+            for (i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            for (i = 0; i < N; i++)
+            {
+                Console.WriteLine(vet[i].ToString("F1", CultureInfo.InvariantCulture));
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
